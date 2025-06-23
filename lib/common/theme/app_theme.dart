3fad031d7ed2cd9111a78f311bd1b70404243625
Clashkid155 <beluchukwunjike@gmail.com>
2025-06-23 11:50:55 +0100

    Create theme for light and dark mode.

--- lib/common/theme/app_theme.dart
+++ lib/common/theme/app_theme.dart
@@ -3,11 +3,69 @@ import 'package:flutter/material.dart';
 class AppTheme {
   AppTheme._();
 
-  static ThemeData lightTheme (){
-    return ThemeData();
+  static ThemeData lightTheme() {
+    return ThemeData(
+      brightness: Brightness.light,
+      primaryColor: Colors.blue,
+      scaffoldBackgroundColor: Colors.white,
+      appBarTheme: const AppBarTheme(
+        color: Colors.blue,
+        iconTheme: IconThemeData(color: Colors.white),
+        titleTextStyle: TextStyle(
+            color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+      ),
+      textTheme: const TextTheme(
+        bodyLarge: TextStyle(color: Colors.black),
+        bodyMedium: TextStyle(color: Colors.black87),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+        ),
+      ),
+    );
   }
 
-  static ThemeData darkTheme (){
-    return ThemeData();
+  static ThemeData darkTheme() {
+    return ThemeData(
+      brightness: Brightness.dark,
+      primaryColor: Colors.teal,
+      scaffoldBackgroundColor: Colors.grey[900],
+      appBarTheme: AppBarTheme(
+        color: Colors.teal,
+        iconTheme: const IconThemeData(color: Colors.white),
+        titleTextStyle: const TextStyle(
+            color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+      ),
+      textTheme: const TextTheme(
+        bodyLarge: TextStyle(color: Colors.white),
+        bodyMedium: TextStyle(color: Colors.white70),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.teal,
+          foregroundColor: Colors.white,
+        ),
+      ),
+    );
+  }
+}
+
+class ThemeProvider with ChangeNotifier {
+  bool _isDarkTheme = false;
+
+  bool get isDarkTheme => _isDarkTheme;
+
+  set isDarkTheme(bool value) {
+    _isDarkTheme = value;
+    notifyListeners();
+  }
+
+  ThemeData get theme =>
+      _isDarkTheme ? AppTheme.darkTheme() : AppTheme.lightTheme();
+
+  void toggleTheme() {
+    isDarkTheme = !_isDarkTheme;
   }
 }

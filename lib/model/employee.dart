97f3107e1344f2a08852b9635c345b50fdbb0215
Clashkid155 <beluchukwunjike@gmail.com>
2025-06-26 02:57:23 +0100

    Add Levels enum.

--- lib/model/employee.dart
+++ lib/model/employee.dart
@@ -87,6 +87,34 @@ enum Designation {
   });
 
   final String label;
+
+  static List<String> get labels => values.map((e) => e.label).toList();
+}
+
+enum Level {
+  level0(label: 'Level 0', pay: 70_000),
+  level1(label: 'Level 1', pay: 100_000),
+  level2(label: 'Level 2', pay: 120_000),
+  level3(label: 'Level 3', pay: 180_000),
+  level4(label: 'Level 4', pay: 200_000),
+  level5(label: 'Level 5', pay: 250_000);
+
+  final String label;
+  final int pay;
+
+  const Level({required this.label, required this.pay});
+
+  static List<String> get labels => values.map((e) => e.label).toList();
+
+  static Level fromString(String label) => switch (label) {
+        'Level 0' => Level.level0,
+        'Level 1' => Level.level1,
+        'Level 2' => Level.level2,
+        'Level 3' => Level.level3,
+        'Level 4' => Level.level4,
+        'Level 5' => Level.level5,
+        _ => throw ArgumentError('Invalid level: $label'),
+      };
 }
 
 enum EmploymentStatus {
